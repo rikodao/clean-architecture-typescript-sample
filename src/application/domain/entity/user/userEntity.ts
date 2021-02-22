55f9c649e84e30cadf29cf3145569a6837f2d7a8
oiso <oiso@CA-PC-1033.local>
2021-02-23 00:37:09 +0900

    User ID 機能変更

--- src/application/domain/entity/user/userEntity.ts
+++ src/application/domain/entity/user/userEntity.ts
@@ -3,49 +3,65 @@ import Age from "./value/userAgeValue";
 import Name from "./value/userNameValue";
 import { stringify } from "uuid";
 import { UserData } from "../../../types/user";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+@Entity()
 export default class UserEntity {
-    private _id: Id
-    private _familyName: Name
-    private _firstName: Name
-    private _age: Age
+  @PrimaryGeneratedColumn()
+  private _id: Id
+  @Column()
+  private _familyName: Name
+  @Column()
+  private _firstName: Name
+  @Column()
+  private _age: Age
 
 
-    constructor(params: { familyName: string, firstName: string, age: number }) {
-        try {
-            this.familyName = params.familyName
-            this.firstName = params.firstName
-            this.age = params.age
-        } catch (error) {
-            throw error
-        }
-        this._id = new Id()
+  constructor(params: { id?: string, familyName: string, firstName: string, age: number }) {
+    try {
+      this.familyName = params.familyName
+      this.firstName = params.firstName
+      this.age = params.age
+      this.id = params.id
+    } catch (error) {
+      throw error
     }
 
-    private set age(age: number) {
-        try {
-            this._age = new Age(age)
-        } catch (error) {
-            throw error
-        }
-    }
+  }
 
-    private set firstName(firstName: string) {
-        try {
-            this._firstName = new Name(firstName)
-        } catch (error) {
-            throw error
-        }
+  private set id(id: string | undefined) {
+    try {
+      this._id = new Id(id)
+    } catch (error) {
+      throw error
     }
+  }
 
-    private set familyName(familyName: string) {
-        try {
-            this._familyName = new Name(familyName)
-        } catch (error) {
-            throw error
-        }
-    }
 
-    get value(): UserData {
-        return { id: this._id.value, firstName: this._firstName.value, familyName: this._familyName.value, age: this._age.value }
+  private set age(age: number) {
+    try {
+      this._age = new Age(age)
+    } catch (error) {
+      throw error
     }
+  }
+
+  private set firstName(firstName: string) {
+    try {
+      this._firstName = new Name(firstName)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  private set familyName(familyName: string) {
+    try {
+      this._familyName = new Name(familyName)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  get value(): UserData {
+    return { id: this._id.value, firstName: this._firstName.value, familyName: this._familyName.value, age: this._age.value }
+  }
 }
--- src/application/domain/entity/user/value/userIdValue.ts
+++ src/application/domain/entity/user/value/userIdValue.ts
@@ -1,8 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 export default class UserIdValue {
     private _id: string
-    constructor() {
-        this._id = uuidv4()
+    constructor(id?: string) {
+        this._id = id || uuidv4()
+        if (this._id.length !== 36) {
+            throw new Error(`不正なID値です: ${this._id}`);
+        }
     }
 
     public get value(): string {
